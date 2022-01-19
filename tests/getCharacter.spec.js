edac2e53890b21b52b50b0b976edf881a339abff
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-18 21:15:22 -0300

    Requisito 9 OK

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,28 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    //fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBe(undefined);
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toEqual({"class": "Rogue", "name": "Arya Stark", "phrases": ["Not today", "A girl has no name."]});
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toEqual({"class": "Knight", "name": "Brienne Tarth", "phrases": ["Im No Lady, Your Grace.", "I, Brienne Of Tarth, Sentence You To Die."]});
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toEqual({"class": "Necromancer", "name": "Melissandre", "phrases": ["Death By Fire Is The Purest Death.", "For The Night Is Dark And Full Of Terrors."]});
+
     // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('arya')).toEqual({"class": "Rogue", "name": "Arya Stark", "phrases": ["Not today", "A girl has no name."]});
+    expect(getCharacter('brienne')).toEqual({"class": "Knight", "name": "Brienne Tarth", "phrases": ["Im No Lady, Your Grace.", "I, Brienne Of Tarth, Sentence You To Die."]});
+    expect(getCharacter('melissandre')).toEqual({"class": "Necromancer", "name": "Melissandre", "phrases": ["Death By Fire Is The Purest Death.", "For The Night Is Dark And Full Of Terrors."]});
+
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Lucas')).toBe(undefined);
+
   });
 });
